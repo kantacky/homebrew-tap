2ada7d113b549378fd8edd6e028e0e4a1f460ced
Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>
2026-06-29 10:44:44 +0900

    Fix

---
+++ Formula/gtfs.rb
@@ -0,0 +1,20 @@
+class GTFS < Formula
+  desc "GTFS CLI"
+  homepage "https://github.com/kantacky/gtfs"
+  version "0.1.0"
+
+  depends_on macos: :tahoe # Package.swift の .macOS(.v26) に合わせる
+
+  on_arm do
+    url "https://github.com/kantacky/gtfs/releases/download/#{version}/gtfs-#{version}-arm64.tar.gz"
+    sha256 "ca8698ed04f63bdb57850b8db23790cb5256ff208e2f2283cdd14734d8d8f65d"
+  end
+
+  def install
+    bin.install "GTFSCLI" => "gtfs"
+  end
+
+  test do
+    assert_match "gtfs", shell_output("#{bin}/gtfs --help")
+  end
+end
--- Formula/transcribe.rb
+++ Formula/transcribe.rb
@@ -6,7 +6,7 @@ class Transcribe < Formula
   depends_on macos: :tahoe # Package.swift の .macOS(.v26) に合わせる
 
   on_arm do
-    url "https://github.com/kantacky/transcribe/releases/download/v#{version}/transcribe-#{version}-arm64.tar.gz"
+    url "https://github.com/kantacky/transcribe/releases/download/#{version}/transcribe-#{version}-arm64.tar.gz"
     sha256 "39a326a54ec3380df272984f26502728507ae3bfc52273f65854ce7c74a0b952"
   end
 
